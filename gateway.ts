deddbecf43ba3fe717c588c92dd6643dc3184626
Abhiram Shukla <shukla.abhiram12@gmail.com>
2026-08-07 22:54:12 +0530

    feat: Added Auth mechanism to gateway
    Also added some personality to the error messages.

--- gateway.ts
+++ gateway.ts
@@ -4,6 +4,18 @@ import { createProxyMiddleware } from "http-proxy-middleware";
 const app = express();
 const PORT = 8080;
 
+const authMiddleware = (req: Request, res: Response, next: NextFunction): void => {
+    const apiKey = req.headers['x-api-key'];
+    const VALID_KEY = 'wubba-lubba-dub-dub';
+
+    if(!apiKey || apiKey !== VALID_KEY) {
+        res.status(401).json({ error: '401 Unauthorized - Credentials or Consequences. Mostly credentials.'});
+        return;
+    }
+
+    next();
+}
+
 const requestCounts = new Map<string, number>();
 const RATE_LIMIT = 5;
 
@@ -12,7 +24,7 @@ const rateLimiter = (req: Request, res: Response, next: NextFunction): void => {
     const currentCount = requestCounts.get(ip) || 0;
 
     if (currentCount >= RATE_LIMIT) {
-        res.status(429).json({ error: 'Rate limit reached. Please try again later.'});
+        res.status(429).json({ error: '429 Too Many Requests - O-oh jeez... The server is trying its best. Maybe slow down a little.'});
         return;
     }
 
@@ -30,7 +42,7 @@ const apiProxy = createProxyMiddleware({
     },
 });
 
-app.use('/api', rateLimiter, apiProxy);
+app.use('/api', authMiddleware, rateLimiter, apiProxy);
 
 app.listen(PORT, () => {
     console.log(`UntitledAPIGateway running on http://localhost:${PORT}`);
